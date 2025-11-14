767605f55b06be555e394594aea7f292cbf3c595
clothingmmtimes-lang <clothingmmtimes@gmail.com>
2025-11-15 03:24:40 +0530

    Update cart.js

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,1 +1,25 @@
+function addToCart(name, price) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    cart.push({ name, price });
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert(name + " added to cart!");
+}
+
+function loadCart() {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    let box = document.getElementById("cart-items");
+
+    box.innerHTML = "";
+    cart.forEach((item) => {
+        box.innerHTML += `
+            <div class="cart-item">
+                ${item.name} - ₹${item.price}
+            </div>
+        `;
+    });
+}
+
+if (document.getElementById("cart-items")) {
+    loadCart();
+}
 
